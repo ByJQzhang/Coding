ec0496ec3be54e1ca28e3ea9c3dc04361125f124
MC <zhangjiaqi@MCdeMacBook-Pro.local>
2017-08-31 09:16:59 +0800

    项目上传

---
+++ Coding/Coding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coding
+//
+//  Created by MC on 2017/8/30.
+//  Copyright © 2017年 FCCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
